dd62377b8e45d814ce91f438a399dc319091d4b5
vblandon10 <v.blandon10@uniandes.edu.co>
2018-10-31 18:16:55 -0500

    grafica signal

--- Fourier.py
+++ Fourier.py
@@ -8,4 +8,18 @@ from scipy.interpolate import interp1d
 signal = np.genfromtxt('signal.dat')
 incompletos = np.genfromtxt('incompletos.dat')
 
-#Haga una grafica de los datos de signal.dat y guarde dicha grafica sin mostrarla 
+#Haga una grafica de los datos de signal.dat y guarde dicha grafica sin mostrarla
+
+# columnas que me interesan
+signal = signal[:,[0,-1]]
+xSignal  =signal[:,0]
+ySignal = signal[:,1]
+
+#grafica
+plt.figure()
+fig = plt.gcf()
+plt.plot(xSignal, ySignal )
+plt.grid()
+plt.xlabel('Tiempo');
+plt.ylabel('y')
+fig.savefig('BlandonValentina_signal.pdf')
