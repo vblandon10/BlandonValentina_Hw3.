c060e5a8ce575745644795e8926103d02ef233f3
vblandon10 <v.blandon10@uniandes.edu.co>
2018-10-31 19:08:46 -0500

    transformada fourier.

--- Fourier.py
+++ Fourier.py
@@ -3,29 +3,25 @@ import matplotlib.pyplot as plt
 from scipy.fftpack import ifft
 from scipy.interpolate import interp1d
 
-#Almacene los datos de signal.dat y de incompletos.dat
-
-signal = np.genfromtxt('signal.dat')
-incompletos = np.genfromtxt('incompletos.dat')
-
-#Haga una grafica de los datos de signal.dat y guarde dicha grafica sin mostrarla
-
+signal = np.genfromtxt('signal.dat') # cargar datos
+# los datos vienen con una columna de nans, entonces elijo solo las
 # columnas que me interesan
 signal = signal[:,[0,-1]]
-xSignal  =signal[:,0]
-ySignal = signal[:,1]
 
-#grafica
+
+# Graficas
 plt.figure()
 fig = plt.gcf()
-plt.plot(xSignal, ySignal )
-plt.title("Grafica de signal")
+plt.plot(signal[:,0], signal[:,1])
+plt.title("Grafica datos signal")
 plt.grid()
 plt.xlabel('Tiempo')
 plt.ylabel('y')
 fig.savefig('BlandonValentina_signal.pdf')
 
 
+
+# transformadas de Fourier, con frecuencias positivas y negativas.
 signal_F = []
 signal_frecuencias_positivas = []
 signal_frecuencias_negativas = []
@@ -42,4 +38,26 @@ for i in range(len(signal)):
 
     if i < len(signal)/2: # Crear vector de frecuencias
         signal_frecuencias_positivas.append( i*signal_fbase )
-        signal_frecuencias_negativas.append( -i*signal_fbase )
+        signal_frecuencias_negativas.append( -i*signal_fbase
+        )
+# concatenar las frecuencias positivas y las negativas en el orden correspondiente
+signal_frecuencias = signal_frecuencias_positivas + list(reversed(signal_frecuencias_negativas))
+
+# Convertir a arrays de numpy
+signal_frecuencias = np.array(signal_frecuencias)
+signal_F = np.array(signal_F)
+
+
+# Crear una copia para hacer el filtro
+signal_F_copia = signal_F.copy()
+
+# Graficas
+#Haga una grafica de la transformada de Fourier y guarde dicha grafica
+plt.figure()
+fig = plt.gcf()
+plt.semilogy(signal_frecuencias, np.abs(signal_F))
+plt.title("Grafica de la transformada")
+plt.grid()
+plt.xlabel('Frecuencias')
+plt.ylabel('Magnitud')
+fig.savefig('BlandonValentina_TF.pdf')
